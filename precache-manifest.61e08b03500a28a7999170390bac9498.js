c661fbe27c650ade151b976943b13a9a17855a9e
Allison DiNapoli <adinapol@asu.edu>
2018-10-08 21:09:29 -0700

    Updates

---
+++ precache-manifest.61e08b03500a28a7999170390bac9498.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "01fc2b661906bc9d2548",
+    "url": "/react-tutorial/static/js/main.01fc2b66.chunk.js"
+  },
+  {
+    "revision": "ccf717e8f1c052517a1a",
+    "url": "/react-tutorial/static/js/1.ccf717e8.chunk.js"
+  },
+  {
+    "revision": "01fc2b661906bc9d2548",
+    "url": "/react-tutorial/static/css/main.42f23724.chunk.css"
+  },
+  {
+    "revision": "74be7146c8aa0924226be624a849c148",
+    "url": "/react-tutorial/index.html"
+  }
+];
